1a2b9ce4bb256daccb996e3fdfbfd4db333cf3b3
esraaamal <ma.esraa@hotmail.com>
2020-03-03 02:01:31 +0200

    g

--- app.js
+++ app.js
@@ -1,32 +1,5 @@
 'use strict';
 
-
-/*
-  Practice domain modeling by planning out an app w that allows users to choose their favorite between two myImages
-  Let students participate by suggesting the steps needed to make the app run
-  App Flow:
-  - Welcome and instructions
-  - Randomly put 2 myImages on the screen
-    - Random number generator
-    - a function to display myImages
-  - A user clicks on a Bus
-    - event listener needs to be on the image to fire the event handler
-    - the event handler firesx
-      - ? check if total clicks is 5 ?
-      - stop letting the user click
-    - if the user reach 5 tries remove image section for myImages and display to the user you fininshed.
-  HTML
-    - have a left and right image container in the html
-    - Give them id's so we can select them
-  We need an Array to hold all image Objects
-  function to randomly pick an image{
-  }
-  click on an image, targetted by id
-  add event listener('click', function(){
-  })
-*/
-
-
 var leftImageRandom;
 var rightImageRandom;
 var centerImageRandom;
@@ -80,17 +53,10 @@ function pickRandomImages() {
   rightImageRandom = myImages[randomNumber(0, myImages.length - 1)];
 
   centerImageRandom = myImages[randomNumber(0, myImages.length - 1)];
-<<<<<<< HEAD
-
-  while ( leftImageRandom === rightImageRandom||  leftImageRandom === centerImageRandom ||rightImageRandom === centerImageRandom) {
-
-
-=======
 
   while (leftImageRandom === rightImageRandom || leftImageRandom === centerImageRandom || rightImageRandom === centerImageRandom) {
 
 
->>>>>>> lab-011
     leftImageRandom = myImages[randomNumber(0, myImages.length - 1)];
 
     rightImageRandom = myImages[randomNumber(0, myImages.length - 1)];
@@ -123,24 +89,6 @@ pickRandomImages();
 // the allImages array is a property of the GoatPicture constructor
 
 function clickImage(e) {
-<<<<<<< HEAD
-  if (e.target.id === 'myLeftPic' || e.target.id === 'myRightPic'||e.target.id === 'myCenterPic') {
-    pickRandomImages();
-
-
-   
-    totalClicks++;
-
-  }
-     if(e.target.id === 'myLeftPic'){
-
-      leftImageRandom.clicks++
-
-    
-
-  }  if (e.target.id === 'myRightPic') {
-    
-=======
   if (e.target.id === 'myLeftPic' || e.target.id === 'myRightPic' || e.target.id === 'myCenterPic') {
     pickRandomImages();
 
@@ -157,22 +105,10 @@ function clickImage(e) {
 
   } if (e.target.id === 'myRightPic') {
 
->>>>>>> lab-011
 
     rightImageRandom.clicks++;
 
 
-<<<<<<< HEAD
-    
-  }  if (e.target.id === 'myCenterPic') {
-   
-
-    centerImageRandom.clicks++;
-
-    
-  }
-   if (totalClicks === 25) {
-=======
 
   } if (e.target.id === 'myCenterPic') {
 
@@ -182,20 +118,10 @@ function clickImage(e) {
 
   }
   if (totalClicks === 25) {
->>>>>>> lab-011
     //remove event listener
     leftBusImg.remove();
     rightBusImg.remove();
     centerBusImg.remove();
-<<<<<<< HEAD
-    
-    renderResult();
-    groupImageSection.removeEventListener('click', clickImage);
-    
-
-
-   
-=======
 
     renderResult();
     groupImageSection.removeEventListener('click', clickImage);
@@ -203,7 +129,6 @@ function clickImage(e) {
 
 
 
->>>>>>> lab-011
   }
 }
 
@@ -212,22 +137,6 @@ function clickImage(e) {
 groupImageSection.addEventListener('click', clickImage);
 
 
-<<<<<<< HEAD
-function renderResult(){
-  var ulReult =document.getElementById("totalResult")
-  for(var i=0 ;i<myImagesImages.length ;i++ ){
-
-var reslutLi =document.createElement('li');
-ulReult.appendChild(reslutLi);
-reslutLi.textContent= ` The ${myImages[i].name} had ${myImages[i].clicks} clicks`;
-var resluthr =document.createElement('hr');
-ulReult.appendChild(resluthr);
-
-  }
-  
-  
-  }
-=======
 function renderResult() {
   var ulReult = document.getElementById("totalResult")
   for (var i = 0; i < myImagesImages.length; i++) {
@@ -242,7 +151,6 @@ function renderResult() {
 
 
 }
->>>>>>> lab-011
 
 
 function randomNumber(min, max) {
