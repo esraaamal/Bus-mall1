22cf5b47f1b5fb55a21bfce643d0c2bca17fb485
esraaamal <ma.esraa@hotmail.com>
2020-03-04 07:06:38 +0200

    df

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-'use strict';
-
 var leftImageRandom;
 var rightImageRandom;
 var centerImageRandom;
-var newArray = [];
+var newArr = [];
+//getItem();
 var myImagesImages = [
   'bag.jpg',
   'banana.jpg',
@@ -31,57 +30,115 @@ var leftBusImg = document.querySelector('#myLeftPic');
 var rightBusImg = document.querySelector('#myRightPic');
 var centerBusImg = document.querySelector('#myCenterPic');
 var groupImageSection = document.getElementById('allBusMall');
+var orders = document.getElementById('orders');
 
-var myImages = [];//an array to store all myImages object
+var myImages = [];
 var totalClicks = 1;
 
 
 
-function Bus(urlImage) {
-  this.name = urlImage.split('.')[0];
-  this.urlImage = `images/${urlImage}`;
+function Bus(name) {
+  this.name = name.split('.')[0];
+  this.urlImage = `images/${name}`;
   this.view = 0;
   this.clicks = 0;
+
   myImages.push(this);//this its refer to the object that im created
-  console.log(myImages);
+  //console.log(myImages);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function pickRandomImages() {
-  console.log(myImages);
-  leftImageRandom = myImages[randomNumber(0, myImages.length - 1)];
 
-  rightImageRandom = myImages[randomNumber(0, myImages.length - 1)];
-
-  centerImageRandom = myImages[randomNumber(0, myImages.length - 1)];
-
-  while (leftImageRandom === rightImageRandom || leftImageRandom === centerImageRandom || rightImageRandom === centerImageRandom) {
+  var myRandomIdex1 = randomNumber(0, myImages.length - 1);
+  leftImageRandom = myImages[myRandomIdex1];
+  myImages.splice(myRandomIdex1, 1);
 
 
-    leftImageRandom = myImages[randomNumber(0, myImages.length - 1)];
+  var myRandomIdex2 = randomNumber(0, myImages.length - 1);
+  rightImageRandom = myImages[myRandomIdex2];
+  myImages.splice(myRandomIdex2, 1);
 
-    rightImageRandom = myImages[randomNumber(0, myImages.length - 1)];
+  var myRandomIdex3 = randomNumber(0, myImages.length - 1);
+  centerImageRandom = myImages[myRandomIdex3];
+  myImages.splice(myRandomIdex3, 1);
 
-    centerImageRandom = myImages[randomNumber(0, myImages.length - 1)]
+
+
+  /*if(myImages.length == 0){
+    myImages =newArr.concat(newArray);
+   
+    var myRandomIdex1=randomNumber(0, myImages.length - 1);
+   leftImageRandom=myImages[myRandomIdex1];
+   myImages.splice(myRandomIdex1 ,1);
+   
+  
+   var myRandomIdex2=randomNumber(0,myImages.length - 1);
+   rightImageRandom=myImages[myRandomIdex2];
+   myImages.splice(myRandomIdex2 ,1);
+  
+   var myRandomIdex3 =randomNumber(0,myImages.length - 1);
+   centerImageRandom =myImages[myRandomIdex3];
+   myImages.splice(myRandomIdex3 ,1);
+  };*/
+  var savedViews = [];
+  function setItem() {
+  
+      for (var i = 0; i <myImages.length; i++) {
+          savedViews[i] = savedViews[i] +myImages[i].view;
+         myImages[i].view = savedViews[i];
+      }
+      localStorage.setItem('esraa', JSON.stringify(myImages));
+  
   }
 
+  function getItem() {
+    var myGetItem= localStorage.getItem('esraa');
+    myImages= JSON.parse(myGetItem);
+    renderOrders()
+}
 
 
   leftBusImg.setAttribute('src', leftImageRandom.urlImage);
   leftBusImg.setAttribute('alt', leftImageRandom.name);
+
   rightBusImg.setAttribute('src', rightImageRandom.urlImage);
   rightBusImg.setAttribute('alt', rightImageRandom.name);
 
   centerBusImg.setAttribute('src', centerImageRandom.urlImage);
   centerBusImg.setAttribute('alt', centerImageRandom.name);
-
+  myImages.push(leftImageRandom);
+  myImages.push(rightImageRandom);
+  myImages.push(centerImageRandom);
 
 }
 
 for (var i = 0; i < myImagesImages.length; i++) {
   console.log(myImagesImages[i]);
   new Bus(myImagesImages[i]);
+  renderOrders();
 
 }
+
+// var newArray =newArr.concat(myImages);
+// console.log(newArray);
+
+
 pickRandomImages();
 
 
@@ -92,16 +149,19 @@ function clickImage(e) {
   if (e.target.id === 'myLeftPic' || e.target.id === 'myRightPic' || e.target.id === 'myCenterPic') {
     pickRandomImages();
 
+    leftImageRandom.view++;
+    rightImageRandom.view++;
 
 
+    centerImageRandom.view++;
     totalClicks++;
 
   }
   if (e.target.id === 'myLeftPic') {
 
-    leftImageRandom.clicks++
+    leftImageRandom.clicks++;
 
-
+    console.log('click', leftImageRandom.clicks);
 
   } if (e.target.id === 'myRightPic') {
 
@@ -119,12 +179,15 @@ function clickImage(e) {
   }
   if (totalClicks === 25) {
     //remove event listener
+    groupImageSection.removeEventListener('click', clickImage);
+
     leftBusImg.remove();
     rightBusImg.remove();
     centerBusImg.remove();
+    renderChartresult();
+    setItem();
 
-    renderResult();
-    groupImageSection.removeEventListener('click', clickImage);
+    //renderResult();
 
 
 
@@ -137,22 +200,98 @@ function clickImage(e) {
 groupImageSection.addEventListener('click', clickImage);
 
 
-function renderResult() {
-  var ulReult = document.getElementById("totalResult")
-  for (var i = 0; i < myImagesImages.length; i++) {
+// function renderResult() {
+//   var ulReult = document.getElementById("totalResult")
+//   for (var i = 0; i < myImagesImages.length; i++) {
 
-    var reslutLi = document.createElement('li');
-    ulReult.appendChild(reslutLi);
-    reslutLi.textContent = ` The ${myImages[i].name} had ${myImages[i].clicks} clicks`;
-    var resluthr = document.createElement('hr');
-    ulReult.appendChild(resluthr);
+//     var reslutLi = document.createElement('li');
+//     ulReult.appendChild(reslutLi);
+//     reslutLi.textContent = ` The ${myImages[i].name} had ${myImages[i].clicks} clicks ,and view for ${myImages[i].view}`;
+//     var resluthr = document.createElement('hr');
+//     ulReult.appendChild(resluthr);
 
-  }
+//   }
 
 
-}
+// }
 
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+// now we will start the chart
+function renderChartresult() {
+  var busMallName = [];//pass this array to x-axis
+  var busClick = [];//we will pass this array in the y-axis
+  var myViews =[];
+  for (var i = 0; i < myImages.length; i++) {
+
+    busMallName.push(myImages[i].name);
+    busClick.push(myImages[i].clicks);
+    myViews.push(myImages[i].view);
+
+
+  }
+
+
+var ctx = document.getElementById('myBusMallC').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: busMallName,
+    datasets: [{
+      label: '# of clicks',
+      data: busClick,
+      backgroundColor:
+        'rgba(255, 99, 132)',
+
+      borderColor:
+        'rgba(255, 99, 132,1)',
+
+
+      borderWidth: 1
+    },
+    {label: '# of Views',
+    data: myViews,
+    backgroundColor:
+      'rgba(50, 8, 12)',
+
+    borderColor:
+      'rgba(50, 8, 12, 1)',
+
+
+    borderWidth: 1
+  }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+////now we  End paste the code of the chartjs
+////////////////////////////////////////////
+////////////////////////////////////////////
+}
+function renderOrders(){
+  // clear all my current uls to prevent duplicate information
+  orders.textContent = "";
+
+  // go through the array and output the details of each drink in the array
+  for(var i=0; i < myImages.length; i++){
+    var drinkLI = document.createElement('li');
+    var infoP = document.createElement('p');
+    var temp;
+    
+    infoP.textContent = `${myImages[i].name} orderd a ${temp} ${myImages[i].clicks} ${myImages[i].view} `;
+    drinkLI.appendChild(infoP);
+    orders.appendChild(drinkLI);
+  }
+}
+
+groupImageSection.addEventListener('busOrder', clickImage)
+getItem();
